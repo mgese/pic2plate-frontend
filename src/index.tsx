f0ccde6ba2e2a21b3fad2e8c4feb660c2c825ec2
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-04 08:16:14 +0200

    :tada: Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import AppWrapper from './components/AppWrapper';
+
+try {
+    const root = createRoot(document.querySelector('#root'));
+    root.render(<AppWrapper />);
+} catch (e) {
+    console.error('Encountered error at `ReactDOM.render`: ', e);
+}
+
