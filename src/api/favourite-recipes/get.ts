34e80ac09c7c82a57163457c5ec8de9b19d78c6a
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-05 11:52:13 +0200

    ✨ Add request urls

--- src/api/favourite-recipes/get.ts
+++ src/api/favourite-recipes/get.ts
@@ -1,3 +1,4 @@
+import { getUser } from 'chayns-api';
 import { request } from '../../utils/request';
 import { ApiFunctionResult } from '../../types/api';
 import { IRecipe } from '../../types/Recipe';
@@ -5,9 +6,11 @@ import { IRecipe } from '../../types/Recipe';
 export const getFavouriteRecipes = async (): Promise<
     ApiFunctionResult<IRecipe[]>
 > => {
+    const user = getUser();
+
     const response = await request<IRecipe[]>({
         method: 'GET',
-        route: ``,
+        route: `recipe/${user?.personId ?? ''}`,
     });
 
     return { data: response.data, status: response.status };
--- src/api/favourite-recipes/post.ts
+++ src/api/favourite-recipes/post.ts
@@ -1,14 +1,26 @@
+import { getUser } from 'chayns-api';
 import { request } from '../../utils/request';
 import { ApiFunctionResult } from '../../types/api';
 import { IRecipe } from '../../types/Recipe';
 
+interface PostRecipeBody {
+    title: string;
+    recipe: string;
+    personId: string;
+}
+
 export const postRecipe = async (
     recipe: IRecipe
 ): Promise<ApiFunctionResult<number>> => {
-    const response = await request<number, IRecipe>({
+    const user = getUser();
+
+    const response = await request<number, PostRecipeBody>({
         method: 'POST',
-        body: recipe,
-        route: ``,
+        body: {
+            personId: user?.personId ?? '',
+            ...recipe,
+        },
+        route: `recipe`,
     });
 
     return { data: response.data, status: response.status };
