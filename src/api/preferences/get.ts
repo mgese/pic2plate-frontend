6154f90b023c08d097833ef450b87a1d619cf17c
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-05 11:55:49 +0200

    ✨ Add map preference result

--- src/api/preferences/get.ts
+++ src/api/preferences/get.ts
@@ -2,12 +2,18 @@ import { getUser } from 'chayns-api';
 import { request } from '../../utils/request';
 import { ApiFunctionResult } from '../../types/api';
 
+interface GetPreferenceResult {
+    id: number;
+    name: string;
+    deletionTime: string;
+}
+
 export const getPreferences = async (): Promise<
-    ApiFunctionResult<string[]>
+    ApiFunctionResult<GetPreferenceResult[]>
 > => {
     const user = getUser();
 
-    const response = await request<string[]>({
+    const response = await request<GetPreferenceResult[]>({
         method: 'GET',
         route: `Preference/${user?.personId ?? ''}`,
     });
